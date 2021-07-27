b078e027029ab2e1563ba6fd4da6c3392bf965f6
Denis Zavedeev <happy96538@gmail.com>
2021-07-27 22:48:40 +0300

    Minimal maven plugin

---
+++ elijars-maven-plugin/src/main/java/com/caco3/elijars/maven/ElijarsMojo.java
@@ -0,0 +1,30 @@
+package com.caco3.elijars.maven;
+
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+
+import java.io.File;
+
+@Mojo(name = "elijars")
+public class ElijarsMojo extends AbstractMojo {
+    @Parameter(defaultValue = "${project}", required = true)
+    private MavenProject mavenProject;
+    @Parameter(defaultValue = "${project.build.outputDirectory}", required = true)
+    private File outputDirectory;
+
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        if (!mavenProject.getPackaging().equals("jar")) {
+            getLog().debug("Skipping execution for non-jar artifacts");
+            return;
+        }
+
+        Artifact artifact = mavenProject.getArtifact();
+        getLog().info("artifact.getFile() = " + artifact.getFile());
+    }
+}
